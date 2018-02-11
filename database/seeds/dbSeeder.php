aea9b38b421e58a6ba2e4b0e53dde23087c57a8f
Zaherkhirullah <zahir.hayrallah@gmail.com>
2018-02-11 21:32:10 +0300

    edit dbseeder

--- database/seeds/dbSeeder.php
+++ database/seeds/dbSeeder.php
@@ -15,12 +15,26 @@ class dbSeeder extends Seeder
         $role->name='it';
         $role->slug='It';
         $role->save();
+        $role = new \App\Http\Models\role();
         $role->name='user';
         $role->slug='User';
         $role->save();
+        $role->save();
+        $role = new \App\Http\Models\role();
         $role->name='admin';
         $role->slug='Admin';
         $role->save();
+      
+        $user = new \App\User();
+        $user->first_name='zaher';
+        $user->last_name='khirullah';
+        $user->username='zaherkhirullah';
+        $user->email='zahir.hayrallah@gmail.com';
+        $user->role_id= 3;
+        $user->affiliate_id= str_random(7);
+        $user->password=bcrypt('Zz96321//');
+        $user->save();
+      
        
     }
 }
