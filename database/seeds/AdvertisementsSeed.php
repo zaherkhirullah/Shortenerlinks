05d80c273642fc58de4d656e1fc5aef12c76d5cb
Zaherkhirullah <zahir.hayrallah@gmail.com>
2018-02-20 23:43:42 +0300

    update proje

--- database/seeds/AdvertisementsSeed.php
+++ database/seeds/AdvertisementsSeed.php
@@ -13,32 +13,32 @@ class AdvertisementsSeed extends Seeder
     {
         $adv = new \App\Http\Models\advertisements();
         $adv->name='ad_link_1';
-        $adv->value='';
+        $adv->value='<script data-cfasync="false" type="text/javascript" src="//p220333.clksite.com/adServe/banners?tid=IF1OUO_300X250"></script>';
         $adv->rank=1;
         $adv->save();
         $adv = new \App\Http\Models\advertisements();
         $adv->name='ad_link_2';
-        $adv->value='';
+        $adv->value='<script type="text/javascript" src="https://toro-tags.com/_tags/jstags.js?s=mx/ouo/300250"></script>';
         $adv->rank=2;
         $adv->save();
         $adv = new \App\Http\Models\advertisements();
         $adv->name='ad_link_3';
-        $adv->value='';
+        $adv->value='<script data-cfasync="false" type="text/javascript" src="//p220333.clksite.com/adServe/banners?tid=IF1OUO_300X250"></script>';
         $adv->rank=3;
         $adv->save();
         $adv = new \App\Http\Models\advertisements();
         $adv->name='ad_file_1';
-        $adv->value='';
+        $adv->value='<script type="text/javascript" src="https://toro-tags.com/_tags/jstags.js?s=mx/ouo/300250"></script>';
         $adv->rank=1;
         $adv->save();
         $adv = new \App\Http\Models\advertisements();
         $adv->name='ad_file_2';
-        $adv->value='';
+        $adv->value='<script data-cfasync="false" type="text/javascript" src="//p220333.clksite.com/adServe/banners?tid=IF1OUO_300X250"></script>';
         $adv->rank=2;
         $adv->save();
         $adv = new \App\Http\Models\advertisements();
         $adv->name='ad_file_3';
-        $adv->value='';
+        $adv->value='<script type="text/javascript" src="https://toro-tags.com/_tags/jstags.js?s=mx/ouo/300250"></script>';
         $adv->rank=3;
         $adv->save();
     }
