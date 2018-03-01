6a458e1b3e48d39820f8b692ea80f695cb82de0a
Zaherkhirullah <zahir.hayrallah@gmail.com>
2018-03-01 23:51:54 +0300

    translate some pages

--- resources/views/visitor/Fcaptcha.blade.php
+++ resources/views/visitor/Fcaptcha.blade.php
@@ -20,18 +20,13 @@
                <h4>@lang('lang.m_check_capatcha')</h4>
                <div id="captcha" class="center-captcha"></div>
                   {{ csrf_field() }}
-               <div style="width: 302px; height: 422px; position: relative;">
-                  <div style="width: 302px; height: 422px; position: absolute;">
-                     <iframe src="https://www.google.com/recaptcha/api/fallback?k={{$site_key}}&amp;hl=ar&amp;v=v1517812337239&amp;t=3&amp;ff=true" frameborder="0" scrolling="no" style="width: 302px; height: 422px; border-style: none;">
-                     </iframe>
-                  </div>
-               </div>
                <div style="border-style: none; bottom: 12px; left: 25px; margin: 0px; padding: 0px; right: 25px; background: #f9f9f9; border: 1px solid #c1c1c1; border-radius: 3px; height: 60px; width: 300px;">
                <textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid #c1c1c1; margin: 10px 25px; padding: 0px; resize: none; ">
                </textarea>
                </div>
                </div>
-               <input class="btn btn-main" type="submit" value="Submit">
+        <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&amp;render=explicit&amp;fallback=true" async="" defer=""></script>
+        <input class="btn btn-main" type="submit" value="Submit">
                 
         </form>
         <div style="width: 300px; margin: 0 auto;">
