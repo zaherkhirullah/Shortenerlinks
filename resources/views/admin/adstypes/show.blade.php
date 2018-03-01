d16ee5e42a13a3c5f3bf4ed960db3414fbfd9576
Zaherkhirullah <zahir.hayrallah@gmail.com>
2018-03-02 01:54:01 +0300

    translate some pages

--- resources/views/admin/adstypes/show.blade.php
+++ resources/views/admin/adstypes/show.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.adlayout')
-@section('title','{{$adstype->name}}  @lang('lang.Details') ')
+@section('title'," {{$adstype->name}} @lang('lang.Details')")
 
 @section('content')
 <div class="col-md-12">
