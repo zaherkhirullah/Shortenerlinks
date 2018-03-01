7acf23fbb500c0659a774461f925731c467acf4d
Zaherkhirullah <zahir.hayrallah@gmail.com>
2018-03-01 23:44:53 +0300

    translate some pages

--- resources/views/visitor/Fcaptcha.blade.php
+++ resources/views/visitor/Fcaptcha.blade.php
@@ -1,13 +1,9 @@
 @extends('layouts.visitor')
-
-@section('content')
-@extends('layouts.visitor')
-
 @section('content')
 <div class="container">
 <div class="col-md-8 col-md-offset-2">
-        <div class="skip-container">
-        <div class="text-center">
+   <div class="skip-container">
+      <div class="text-center">
                 <div class="afs_ads">&nbsp;</div>
                 <span id="msg-adblock" class="msg-adblock"></span>
                 <div style="width: 300px; margin: 0 auto;">
@@ -26,16 +22,15 @@
                 
                 <div id="captcha" class="center-captcha"></div>
                         {{ csrf_field() }}
-        <div style="width: 302px; height: 422px; position: relative;">
-        <div style="width: 302px; height: 422px; position: absolute;">
-        <iframe src="https://www.google.com/recaptcha/api/fallback?k={{$site_key}}&amp;hl=ar&amp;v=v1517812337239&amp;t=3&amp;ff=true" frameborder="0" scrolling="no" style="width: 302px; height: 422px; border-style: none;">
-        </iframe>
-        </div>
-        </div>
+                <div style="width: 302px; height: 422px; position: relative;">
+                        <div style="width: 302px; height: 422px; position: absolute;">
+                                <iframe src="https://www.google.com/recaptcha/api/fallback?k={{$site_key}}&amp;hl=ar&amp;v=v1517812337239&amp;t=3&amp;ff=true" frameborder="0" scrolling="no" style="width: 302px; height: 422px; border-style: none;">
+                                </iframe>
+                        </div>
+                </div>
         <div style="border-style: none; bottom: 12px; left: 25px; margin: 0px; padding: 0px; right: 25px; background: #f9f9f9; border: 1px solid #c1c1c1; border-radius: 3px; height: 60px; width: 300px;">
-                <textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid #c1c1c1; margin: 10px 25px; padding: 0px; resize: none; ">
-                </textarea>
-        </div>
+        <textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid #c1c1c1; margin: 10px 25px; padding: 0px; resize: none; ">
+        </textarea>
         </div>
         </div>
         <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&amp;render=explicit&amp;fallback=true" async="" defer=""></script>
